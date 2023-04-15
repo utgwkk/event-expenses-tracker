6fa771af57e5ac3af0f9028e1976acbe2537f90b
utgwkk <utagawakiki@gmail.com>
2023-04-15 21:57:16 +0900

    preset

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,12 @@ type Expense = {
   createdAt: number;
 };
 
+type Preset = {
+  price: number;
+  // new Date().getTime()
+  createdAt: number;
+};
+
 function App() {
   const [expenses, setExpenses] = useStateWithLocalStorage<Expense[]>(
     [],
@@ -30,11 +36,58 @@ function App() {
     [setExpenses]
   );
 
+  const [presets, setPresets] = useStateWithLocalStorage<Preset[]>(
+    [],
+    "presets"
+  );
+  const addPreset = useCallback(
+    (p: Preset) => {
+      setPresets((curr) => [...curr, p]);
+    },
+    [setPresets]
+  );
+  const [newPresetPrice, setNewPresetPrice] = useState(0);
+
   const [price, setPrice] = useState(0);
 
   return (
     <div className="App">
-      <h1>支出記録</h1>
+      <h2>プリセット</h2>
+      <input
+        type="number"
+        min={0}
+        value={newPresetPrice}
+        onChange={(e) => setNewPresetPrice(Number(e.target.value))}
+      />
+      <button
+        type="button"
+        onClick={() =>
+          addPreset({
+            price: newPresetPrice,
+            createdAt: new Date().getTime(),
+          })
+        }
+      >
+        追加
+      </button>
+      <div>
+        {presets.map((p, i) => (
+          <button
+            key={i}
+            type="button"
+            onClick={() =>
+              addExpense({
+                price: p.price,
+                label: "",
+                createdAt: new Date().getTime(),
+              })
+            }
+          >
+            {p.price}
+          </button>
+        ))}
+      </div>
+      <h2>支出記録</h2>
       <ul>
         <li>
           <input
