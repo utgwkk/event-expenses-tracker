d1a44a88381f652bb1705a77a8e3415afbf5e700
utgwkk <utagawakiki@gmail.com>
2023-04-16 08:21:31 +0900

    refactor DangerZone

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,18 @@
 import React from "react";
-import { Button, Container } from "react-bootstrap";
-import { useSetRecoilState } from "recoil";
-import { expensesAtom } from "./recoil/expenses";
+import { Container } from "react-bootstrap";
 import { Presets } from "./components/Presets";
-import { presetsAtom } from "./recoil/presets";
 import { Expenses } from "./components/Expenses";
 import { CSVExport } from "./components/CSVExport";
+import { DangerZone } from "./components/DangerZone";
 
 function App() {
-  const setExpenses = useSetRecoilState(expensesAtom);
-  const setPresets = useSetRecoilState(presetsAtom);
-
   return (
     <Container className="p-3">
       <h1>イベント支出記録君</h1>
       <Presets />
       <Expenses />
       <CSVExport />
-      <h2>スーパー危険ゾーン</h2>
-      <Button
-        type="button"
-        variant="danger"
-        className="me-1"
-        onClick={() => {
-          if (window.confirm("全部の支出記録を消しますか？")) {
-            if (window.confirm("本当に全部の支出記録を消してよいですか？？")) {
-              setExpenses([]);
-            }
-          }
-        }}
-      >
-        支出記録を全部消す
-      </Button>
-      <Button
-        type="button"
-        variant="danger"
-        className="me-1"
-        onClick={() => {
-          if (window.confirm("全部のプリセットを消しますか？")) {
-            setPresets([]);
-          }
-        }}
-      >
-        プリセットを全部消す
-      </Button>
+      <DangerZone />
     </Container>
   );
 }
---
+++ src/components/DangerZone.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import { useSetRecoilState } from "recoil";
+import { expensesAtom } from "../recoil/expenses";
+import { presetsAtom } from "../recoil/presets";
+
+export const DangerZone: React.FC = () => {
+  const setExpenses = useSetRecoilState(expensesAtom);
+  const setPresets = useSetRecoilState(presetsAtom);
+
+  return (
+    <>
+      <h2>スーパー危険ゾーン</h2>
+      <Button
+        type="button"
+        variant="danger"
+        className="me-1"
+        onClick={() => {
+          if (window.confirm("全部の支出記録を消しますか？")) {
+            if (window.confirm("本当に全部の支出記録を消してよいですか？？")) {
+              setExpenses([]);
+            }
+          }
+        }}
+      >
+        支出記録を全部消す
+      </Button>
+      <Button
+        type="button"
+        variant="danger"
+        className="me-1"
+        onClick={() => {
+          if (window.confirm("全部のプリセットを消しますか？")) {
+            setPresets([]);
+          }
+        }}
+      >
+        プリセットを全部消す
+      </Button>
+    </>
+  );
+};
