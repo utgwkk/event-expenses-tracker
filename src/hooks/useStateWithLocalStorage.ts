d217c9db1a9200d13b6f6ad528e7cb5f4fc8e111
utgwkk <utagawakiki@gmail.com>
2023-04-15 21:30:49 +0900

    useStateWithLocalStorage

---
+++ src/hooks/useStateWithLocalStorage.ts
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const localStorageKey = (key: string) => `useStateWithLocalStorage:${key}`;
+
+export const useStateWithLocalStorage = <T>(
+  defaultValue: T,
+  key: string
+): [T, (next: T | ((curr: T) => T)) => void] => {
+  const [state, setState] = useState<T>(() => {
+    const storedValue = localStorage.getItem(localStorageKey(key));
+    if (storedValue === null) {
+      return defaultValue;
+    }
+
+    return JSON.parse(storedValue);
+  });
+
+  useEffect(() => {
+    localStorage.setItem(localStorageKey(key), JSON.stringify(defaultValue));
+  }, [state]);
+
+  return [state, setState];
+};
