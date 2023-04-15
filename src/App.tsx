3617be0910926d7e23ebf4c03e53f6f457f1928a
utgwkk <utagawakiki@gmail.com>
2023-04-15 22:56:47 +0900

    reset

--- src/App.tsx
+++ src/App.tsx
@@ -188,6 +188,33 @@ function App() {
       <Button type="button" onClick={() => exportAsCSV(expenses)}>
         CSVとしてエクスポート
       </Button>
+      <h2>スーパー危険ゾーン</h2>
+      <Button
+        type="button"
+        variant="danger"
+        className="me-1"
+        onClick={() => {
+          if (window.confirm("全部の支出記録を消しますか？")) {
+            if (window.confirm("本当に全部の支出記録を消してよいですか？？")) {
+              setExpenses([]);
+            }
+          }
+        }}
+      >
+        支出記録を全部消す
+      </Button>
+      <Button
+        type="button"
+        variant="danger"
+        className="me-1"
+        onClick={() => {
+          if (window.confirm("全部のプリセットを消しますか？")) {
+            setPresets([]);
+          }
+        }}
+      >
+        プリセットを全部消す
+      </Button>
     </div>
   );
 }
