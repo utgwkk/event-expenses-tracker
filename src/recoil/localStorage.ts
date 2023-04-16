4b1be45fec3c1aed27c4db539a49d1e6b30c2109
utgwkk <utagawakiki@gmail.com>
2023-04-16 09:29:53 +0900

    prettier

--- src/recoil/localStorage.ts
+++ src/recoil/localStorage.ts
@@ -4,17 +4,21 @@ const localStorageKey = (key: string) =>
   `event-expenses-tracker:useStateWithLocalStorage:${key}`;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const persistToLocalStorage: AtomEffect<any> =
-  ({ node, trigger, setSelf, onSet }) => {
-    const key = node.key
-    if (trigger === "get") {
-      const storedValue = localStorage.getItem(localStorageKey(key));
-      if (storedValue !== null) {
-        setSelf(JSON.parse(storedValue))
-      }
+export const persistToLocalStorage: AtomEffect<any> = ({
+  node,
+  trigger,
+  setSelf,
+  onSet,
+}) => {
+  const key = node.key;
+  if (trigger === "get") {
+    const storedValue = localStorage.getItem(localStorageKey(key));
+    if (storedValue !== null) {
+      setSelf(JSON.parse(storedValue));
     }
+  }
 
-    onSet((newValue) => {
-      localStorage.setItem(localStorageKey(key), JSON.stringify(newValue));
-    });
-  };
+  onSet((newValue) => {
+    localStorage.setItem(localStorageKey(key), JSON.stringify(newValue));
+  });
+};
