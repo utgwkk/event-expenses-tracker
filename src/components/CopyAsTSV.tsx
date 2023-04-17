ad8953fd337c22e7ac4c537688ad62e86c851638
utgwkk <utagawakiki@gmail.com>
2023-04-17 09:46:08 +0900

    copy as TSV

--- src/components/CopyAsTSV.tsx
+++ src/components/CopyAsTSV.tsx
@@ -1,19 +1,15 @@
 import React from "react";
 import { Button } from "react-bootstrap";
-import { exportAsCSV } from "../exportData";
+import { copyAsTSV } from "../exportData";
 import { useRecoilValue } from "recoil";
 import { expensesAtom } from "../recoil/expenses";
 
-export const CSVExport: React.FC = () => {
+export const CopyAsTSV: React.FC = () => {
   const expenses = useRecoilValue(expensesAtom);
 
   return (
-    <Button
-      type="button"
-      className="me-1"
-      onClick={() => exportAsCSV(expenses)}
-    >
-      CSVとしてエクスポート
+    <Button type="button" onClick={() => copyAsTSV(expenses)}>
+      TSVとしてコピー
     </Button>
   );
 };
